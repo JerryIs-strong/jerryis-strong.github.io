a8551059b9ef6c27960f08e70261913646cbb9a1
JerryIs-strong <96361414+JerryIs-strong@users.noreply.github.com>
2023-11-21 15:26:36 +0000

    🎉 INT COMMIT: Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/jp-7533/","posts/jp-28506/","posts/jp-34818/","posts/jp-19539/"];function toRandomPost(){
+var posts=["posts/jp-7533/","posts/jp-33380/","posts/jp-28506/","posts/jp-34818/","posts/jp-19539/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
